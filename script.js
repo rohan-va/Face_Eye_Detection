5b9baed9795e04ae988f47927f70160d3a022101
rohan-va <rohan.y.vaidya@gmail.com>
2025-04-26 01:12:20 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
 }
+
+function openLink() {
+    window.open("https://docs.google.com/document/d/1wcAp2rzVF_N_Ys58pgKpBmwiMytZXHbm8cf9daH_C48/edit?usp=sharing", "_blank");
+}
